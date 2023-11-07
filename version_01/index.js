2bd1ee96a8b8fa86934ed72b2962c2920e09ced6
salinpaul valooran <salinpv@hotmail.com>
2023-11-08 01:41:57 +0530

    event listener on password field

--- version_01/index.js
+++ version_01/index.js
@@ -1,3 +1,5 @@
 let password = document.getElementById("password");
 let message = document.getElementById("message");
 let strength = document.getElementById("strength");
+
+password.addEventListener("input", () => {});
